7a44bcfc44bd8a03596d51e3b4673dd0db11a24d
QP Amanda <qpapple@yahoo.com>
2021-06-17 09:58:05 +0800

    Delete restapi directory

--- restapi/main.go
+++
@@ -1,9 +0,0 @@
-// Package main invokes the server package to start and run the REST API.
-package main
-
-import "GoMS1Assignment/restapi/server"
-
-// main calls server.StartServer() to run the application.
-func main() {
-	server.StartServer()
-}
